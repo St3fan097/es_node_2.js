a75dd1ff20308556cdc4ea0eb1763e700c143de0
St3fan097 <Ste97.esse@gmail.com>
2024-01-21 20:05:42 +0100

    exercise-13

--- esercizi/es-12/src/server.ts
+++ esercizi/es-12/src/server.ts
@@ -31,9 +31,31 @@ let planets: Planets = [
     },
 ];
 //metodo
-app.get("/", (req, res) => {
+app.get("/api/planets", (req, res) => {
     res.status(200).json(planets);
-});
+})
+app.get("/api/planets/:id", (req, res) => {
+    const planetId = Number(req.params.id)
+    const planet = planets.find((p) => p.id === planetId);
+    res.status(200).json(planet);
+})
+app.post("/api/planets", (req, res) => {
+    const { id, name } = req.body;
+    const newPlanet = { id, name };
+    planets = [...planets, newPlanet];
+    res.status(201).json({ msg: "Post completed" });
+})
+app.put("/api/planets/:id", (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    planets = planets.map(p => p.id === Number(id) ? ({ ...p, name }) : p)
+    res.status(200).json({ msg: "Post updated" });
+})
+app.delete("/api/planets/:id", (req, res) => {
+    const { id } = req.params;
+    planets = planets.filter(p => p.id !== Number(id));
+    res.status(200).json({ msg: "Post deleted" });
+})
 
 //avvio server
 app.listen(port, () => {
